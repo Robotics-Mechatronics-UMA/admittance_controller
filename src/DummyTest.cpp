5957f743353b06e4d83412499540c16144a0862f
VicRos15 <victorrosillo1110@uma.es>
2024-04-18 13:38:21 +0200

    Update dummytest.cpp

--- src/DummyTest.cpp
+++ src/DummyTest.cpp
@@ -1,5 +1,5 @@
 #include <ros/ros.h>
-#include <geometry_msgs/Vector3.h>
+#include <geometry_msgs/Twist.h>
 #include <cstdlib>
 #include <ctime>
 
@@ -7,34 +7,39 @@ int main(int argc, char **argv)
 {
     ros::init(argc, argv, "force_sensor_node");
     ros::NodeHandle nh;
-    ros::Publisher force_publisher = nh.advertise<geometry_msgs::Vector3>("/ForceSensor", 1);
+    ros::Publisher force_publisher = nh.advertise<geometry_msgs::Twist>("/force", 10);
 
     ros::Rate loop_rate(100); // Frecuencia de publicación en Hz
 
     // Inicializar la variable de tiempo
     ros::Time start_time = ros::Time::now();
 
-    geometry_msgs::Vector3 force_msg;
+    geometry_msgs::Twist msg;
+    msg.angular.x = 0.0;
+    msg.angular.y = 0.0;
+    msg.angular.z = 0.0;
 
     while (ros::ok())
     {
         // Publicar una fuerza de 30N durante 1 segundo
         if ((ros::Time::now() - start_time).toSec() < 1)
         {
-            force_msg.x = 30.0;
-            force_msg.y = 30.0;
-            force_msg.z = 30.0;
+            msg.linear.x = 30.0;
+            msg.linear.y = 30.0;
+            msg.linear.z = 30.0;
 
-            force_publisher.publish(force_msg);
+
+            force_publisher.publish(msg);
         }
         // Publicar una fuerza de 0N durante 1 segundo
         else if ((ros::Time::now() - start_time).toSec() < 2)
         {
-            force_msg.x = 0.0;
-            force_msg.y = 0.0;
-            force_msg.z = 0.0;
+            msg.linear.x = 0.0;
+            msg.linear.y = 0.0;
+            msg.linear.z = 0.0;
 
-            force_publisher.publish(force_msg);
+
+            force_publisher.publish(msg);
         }
         // Reiniciar el temporizador
         else
@@ -42,8 +47,6 @@ int main(int argc, char **argv)
             start_time = ros::Time::now();
         }
 
-        ros::spinOnce();
-        loop_rate.sleep();
     }
 
     return 0;
